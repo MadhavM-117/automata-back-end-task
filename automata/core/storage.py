cbd97be09fe83e6be9dbef0bbec03990ca9f6ef2
Madhav M. <madhavmanoj117@gmail.com>
2025-04-04 10:48:27 +0100

    feat: handle local state storage
    - store state in a way the user doesn't have to worry about it
    - ensure cross-platform support

---
+++ automata/core/storage.py
@@ -0,0 +1,44 @@
+import tempfile
+import traceback
+from os import path
+
+from pydantic import ValidationError
+
+from automata.logging import get_logger
+from automata.models import InternalGameState
+
+logger = get_logger("storage")
+
+
+def get_state_file_path() -> str:
+    return path.join(tempfile.gettempdir(), "automata-game_state.json")
+
+
+def load_game_state() -> InternalGameState:
+    state_file = get_state_file_path()
+    game_state = InternalGameState()
+
+    if path.exists(state_file):
+        with open(get_state_file_path()) as file:
+            content = file.read()
+            try:
+                game_state = InternalGameState.model_validate_json(content)
+            except ValidationError:
+                logger.warning(
+                    f"Failed to load game state from file. {traceback.format_exc()}"
+                )
+            except Exception:
+                logger.warning(f"Unexpected error. {traceback.format_exc()}")
+
+    return game_state
+
+
+def save_game_state(game_state: InternalGameState) -> None:
+    state_file = get_state_file_path()
+    with open(state_file, "w") as file:
+        try:
+            file.write(game_state.model_dump_json())
+        except:
+            logger.error(
+                f"Unexpected error while saving state. {traceback.format_exc()}"
+            )
