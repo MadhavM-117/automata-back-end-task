859628a7e2be308f87017cda177555fd7538f54c
Madhav M. <madhavmanoj117@gmail.com>
2025-04-04 13:01:54 +0100

    fix: handle keyboard interrupts nicely

--- automata/__main__.py
+++ automata/__main__.py
@@ -1,10 +1,27 @@
+import sys
+import time
+
 from automata.logging import setup_logging
 from automata.ui.cli import start_game
 
 
 def main():
     setup_logging()
-    start_game()
+    try:
+        start_game()
+    except KeyboardInterrupt:
+        print("\nOh. Did you want to leave? Too bad!")
+        time.sleep(2)
+        try:
+            start_game()
+        except KeyboardInterrupt:
+            print("\nWell, Goodbye to you too!")
+            sys.exit(0)
+    except Exception:
+        print(
+            "\nWe seem to have run into a tiny bug. We'll be right back with the exterminators."
+        )
+        sys.exit(0)
 
 
 if __name__ == "__main__":
